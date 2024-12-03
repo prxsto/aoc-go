4f6db191f5cc1211e7af5c6a283c8c68f50f6fb5
prxsto <15711032+prxsto@users.noreply.github.com>
2024-12-03 11:46:38 -0800

    add read day2 input file logic

--- 2024/02/code.go
+++ 2024/02/code.go
@@ -27,5 +27,40 @@ func run(part2 bool, input string) any {
 		return "not implemented"
 	}
 
+	path := "input-user.txt"
+	data := getInput(path)
+
+	fmt.Println(data)
+
 	return 42
+}
+
+func getInput(path string) [][]int {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var data [][]int
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		vals := strings.Fields(line)
+
+		var report []int
+		for _, num := range vals {
+			val, err := strconv.Atoi(num)
+			if err != nil {
+				fmt.Println("error converting string: ", err)
+			}
+			report = append(report, val)
+		}
+
+		data = append(data, report)
+
+	}
+	return data
 }
